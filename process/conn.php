acbcc000847ada5e3c955449f0106feff47b3472
Ahmad Alawneh <127956465+AhmadAlawneh3@users.noreply.github.com>
2023-11-24 02:01:18 +0300

    Update conn.php

--- process/conn.php
+++ process/conn.php
@@ -1,9 +1,4 @@
  <?php
-    // $servername = "127.0.0.1";
-    // $user = "root";
-    // $password = "alawneh";
-    // $dbname = "gym";
-
     $conn = mysqli_connect('localhost:3306', 'root', '', 'gym');
     return;
 ?>
