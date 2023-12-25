9a19c402a5b563b830cdc66ddb955a6c0cdf8e0a
AhmadAlawneh3 <a.3alawneh@gmail.com>
2023-12-25 03:24:23 +0300

    Update gym.sql

--- db/gym.sql
+++ db/gym.sql
@@ -37,14 +37,7 @@ CREATE TABLE IF NOT EXISTS `contact_messages` (
   PRIMARY KEY (`mNumber`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `contact_messages`
---
 
-INSERT INTO `contact_messages` (`mNumber`, `name`, `phone`, `email`, `message`) VALUES
-(6, 'dasdasdsadasd', 789634596, 'dsfsdfs`fds@wdasda.com', 'dasdadasda');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `customers`
@@ -72,18 +65,7 @@ CREATE TABLE IF NOT EXISTS `customers` (
 --
 
 INSERT INTO `customers` (`id`, `username`, `fname`, `mname`, `lname`, `DOB`, `gender`, `email`, `phone`, `membership`) VALUES
-(11, 'ahmad', 'ahmad', 'B', 'alawneh', '2003-05-20', 'Male', 'a@a.com', 792834949, 1),
-(12, 'aya', 'aya', 'saleh', 'salman', '2003-05-07', 'Female', 'ayasalman.j@gmail.com', 791312109, 1),
-(13, 'tiger', 'tiger', 'tiger', 'mustafa', '2003-06-25', 'Male', 'mustafa2562003@gmail.com', 776669993, NULL),
-(14, 'ayaaa', 'Aya', 'salman', 'Salman', '2003-05-07', 'f', 'ayasalman.j@gmail.com', 791312108, 1),
-(15, 'abd', 'abdelrahman', 'Kamel', 'Nayfeh', '2023-05-29', 'm', 'nayfehabdelrahman@gmail.com', 790251813, NULL),
-(16, 'abood', 'abood', 'kamel', 'nayfeh', '2023-06-12', 'Male', 'nayfehabdelrahman@gmail.com', 790251813, 3),
-(17, 'mum', 'rasha', 'rasha', 'mustafa', '1984-07-08', 'Female', 'ayasalman.j@gmail.com', 791312108, 1),
-(18, 'abu_haider', 'abu', 'abed', 'haider', '1946-05-25', 'Female', 'abuHaider@gmail.com', 784631928, NULL),
-(19, 'abu_haider1', 'abu', 'abed', 'haider', '1946-05-25', 'Female', 'abuHaider@gmail.com', 784631928, 1),
-(20, 'monkey', 'chipmunk', 'gorilla', 'panda', '2005-06-27', 'Female', 'ramaabuasaad11@gmail.com', 788483931, NULL),
-(21, 'ruba', 'ruba', 'mohd', 'malkawi', '1977-01-14', 'Female', 'malkakssshdb@fdfdj.com', 798185520, 3),
-(22, 'sara123', 'sara', 'tedmori', 'teddmori', '2023-06-10', 'Female', 'a@a.com', 789087668, 1);
+(1, 'ahmad', 'ahmad', 'B', 'alawneh', '2001-08-13', 'Male', 'a@a.com', 791234567, 1);
 
 -- --------------------------------------------------------
 
@@ -160,18 +142,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `is_admin`) VALUES
-(11, 'ahmad', '9c140fcec5136bd1eb5fad96e4201ab9', 1),
-(12, 'aya', 'cfac09994fc224213e63a2fb8336ab92', 1),
-(13, 'tiger', 'f986bff64aa331ab68778a9a4ea3372c', 0),
-(14, 'ayaaa', 'cfac09994fc224213e63a2fb8336ab92', 0),
-(15, 'abd', '7171cbe2c97cbca3dd6cf50cde37d21d', 0),
-(16, 'abood', '8de13959395270bf9d6819f818ab1a00', 0),
-(17, 'mum', '85b688d384b130182207ce4bf923deb7', 0),
-(18, 'abu_haider', '7e2231449eb61daee84bc387360498f7', 0),
-(19, 'abu_haider1', '8e869f6e094efd9e2187b797b46b60bb', 0),
-(20, 'monkey', '5528593b1ccb1598205101c94741638e', 0),
-(21, 'ruba', '1adbb3178591fd5bb0c248518f39bf6d', 0),
-(22, 'sara123', '25d55ad283aa400af464c76d713c07ad', 0);
+(1, 'ahmad', '9c140fcec5136bd1eb5fad96e4201ab9', 1);
 
 --
 -- Constraints for dumped tables
